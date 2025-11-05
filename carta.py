75258ae91deacab138c4b1938249bbd3ee3275dd
2btc04 <2btc04@iesramonllull.local>
2025-11-05 09:49:31 +0100

    creado maid

--- carta.py
+++ carta.py
@@ -1,1 +1,8 @@
 # Class Carta
+
+class Carta:
+    def __init__(self, palo, valor):
+        self.palo= palo
+        self.valor= valor
+    def __repr__(self):
+        return f"{self.valor} de {self.palo}"
